5a47a5db5131742f6791239286f3e303e66fad13
Laurence <ljwoodward.code@gmail.com>
2017-11-17 15:26:27 +0000

    Table class: created instance variables and constructor

--- app/src/main/java/codeclan/com/mariositalianrestaurant/Models/Table.java
+++ app/src/main/java/codeclan/com/mariositalianrestaurant/Models/Table.java
@@ -1,8 +1,22 @@
 package codeclan.com.mariositalianrestaurant.Models;
 
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
 /**
  * Created by janemackay on 17/11/2017.
  */
 
 class Table {
+
+    private ArrayList<Patron> patrons;
+    private ArrayList<LaCarteItem> order;
+
+    public Table() {
+        this.patrons = new ArrayList<Patron>();
+        this.order = new ArrayList<LaCarteItem>();
+    }
+
+
 }
